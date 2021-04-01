10c1e9fdeeae2079cf65e310e9370d503a601758
KeiTaylor0606 <mvemtae5757@outlook.jp>
2021-04-01 15:03:05 +0900

    1000000007

---
+++ 応用的なアルゴリズム/組み合わせと数え上げ問題/TypicalStairs.py
@@ -0,0 +1,26 @@
+#問題
+#https://atcoder.jp/contests/abc129/tasks/abc129_c
+
+N, M = list(map(int, input().split()))
+
+#ok[i] = Trueであればi談目が使える
+ok = [True]*(N + 1)
+for i in range(M):
+    a = int(input())
+    ok[a] = False
+
+MOD = 10**9 + 7
+
+#cnt[i]:i段目までたどり着く移動方法の数
+cnt = [0]*(N + 1)
+#初期条件
+cnt[0] = 1
+
+for i in range(1, N + 1):
+    if ok[i]:
+        if i == 1:
+            cnt[i] = cnt[i - 1]
+        else:
+            cnt[i] = (cnt[i - 1] + cnt[i - 2]) % MOD
+
+print(cnt[N])
